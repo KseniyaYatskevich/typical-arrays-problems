07c59b2070e36afc440d7317760506770c46923e
KseniaYatskevich <kseniayatskevich@gmail.com>
2020-02-19 22:03:53 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+  const sortArray = array.sort((a,b) => a-b);
+  return sortArray[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+  const sortArray = array.sort((a,b) => b-a);
+  return sortArray[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+  return array.reduce((acc,item) => (acc+item),0) / array.length;
 }
